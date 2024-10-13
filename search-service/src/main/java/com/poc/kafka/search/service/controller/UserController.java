b7e761aa59e3c0fb590ef69449d2de54d97ea3ed
jhbafaws <jhbafaws@gmail.com>
2024-10-12 22:20:32 -0300

    add log for search-service

--- search-service/src/main/java/com/poc/kafka/search/service/controller/UserController.java
+++ search-service/src/main/java/com/poc/kafka/search/service/controller/UserController.java
@@ -2,6 +2,8 @@ package com.poc.kafka.search.service.controller;
 
 import com.poc.kafka.search.service.dto.UserDto;
 import com.poc.kafka.search.service.service.ElasticSearchUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,16 +19,21 @@ import java.util.List;
 @RequestMapping("/api/users")
 public class UserController {
 
+    private final static Logger log = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private ElasticSearchUserService userService;
 
     // Endpoint para obtener un usuario por su ID
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable String id) throws IOException {
+
+    log.info("getUserById, PathVariable : {} ", id);
         UserDto user = userService.getUserById(id);
+        log.info("getUserById, getUserByid for elasticSearch : {} ", user);
         if (user != null) {
             return ResponseEntity.ok(user);
         } else {
+            log.error("getUserById, getUserByid  NOT found for elasticSearch : {} ", id);
             return ResponseEntity.notFound().build();
         }
     }
